7656ade29d507af0c652d71c6ed552744bc2db63
Nathalie-Calderon <79361631+Nathalie-Calderon@users.noreply.github.com>
2021-09-02 23:17:47 -0500

    Add files via upload

---
+++ ejercicio3.js
@@ -0,0 +1,10 @@
+/*Ejercicio 3
+Escribe un programa que le pida al usuario ingresar un número, luego le debe pedir un
+segundo número e imprima en la consola la suma de ambos números que ingresó el
+usuario.*/
+var n1 = prompt("Escribe un número");
+var n2 = prompt("Escribe otro número");
+console.log("La suma de " + n1 + " y " + n2 + " es: ");
+console.log(parseInt(n1) + parseInt(n2));
+document.write("La suma de " + n1 + " y " + n2 + " es: ");
+document.write(parseInt(n1) + parseInt(n2));
