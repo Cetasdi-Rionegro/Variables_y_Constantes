09ced60c5ee80b42e86ebe65b5fce792b612104e
Nathalie-Calderon <79361631+Nathalie-Calderon@users.noreply.github.com>
2021-09-02 22:35:28 -0500

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,9 @@
+/*Escribe un programa que le pregunte al usuario su nombre e imprima "Hola, " seguido
+del nombre y un signo de exclamación.
+Por ejemplo si el usuario ingresa "Pedro" el programa debe imprimir en la consola
+"Hola, Pedro!".*/
+var nombre;
+nombre = prompt("Escriba su Nombre");
+console.log("El nombre introducido es: " + nombre);
+var username = nombre;
+alert("Hola " + username + "!");
